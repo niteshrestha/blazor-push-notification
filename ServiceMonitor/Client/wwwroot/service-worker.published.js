8342ad75a320d09cc823736b880e5f603d205824
niteshrestha <4751915+niteshrestha@users.noreply.github.com>
2021-02-16 21:22:27 +0545

    updated published service worker to handle incoming push messaging

--- ServiceMonitor/Client/wwwroot/service-worker.published.js
+++ ServiceMonitor/Client/wwwroot/service-worker.published.js
@@ -46,3 +46,20 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
+
+self.addEventListener('push', event => {
+    const payload = event.data.json();
+    event.waitUntil(
+        self.registration.showNotification('Service Monitor', {
+            body: payload.message,
+            icon: 'icon-512.png',
+            vibrate: [100, 50, 100],
+            data: { url: payload.url }
+        })
+    );
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
